eef9538058b7b743d311ee219fc832350be47ce5
Dymedrol <dmitrykob@gmail.com>
2025-12-18 02:29:39 +0700

    cart update

--- widget_types/le_cart/snippet.js
+++ widget_types/le_cart/snippet.js
@@ -1,9 +1,32 @@
-console.log('@!!!')
-
-EventBus.subscribe('remove_items:insales:cart', function (data) {
-    console.log('Товары удалены');
+EventBus.subscribe('delete_items:insales:cart', function (data) {
+    location.reload();
 });
 
-EventBus.subscribe('delete_items:insales:cart', function (data) {
-    console.log('Товары удалены');
-  });
+EventBus.subscribe('add_items:insales:cart', function (data) {
+    // Перезагружаем страницу после успешного добавления
+    location.reload();
+});
+
+// Обработчик клика на кнопку добавления подарочной упаковки
+document.addEventListener('DOMContentLoaded', function() {
+    var packageButton = document.getElementById('package-product-id-button');
+    
+    if (packageButton) {
+        packageButton.addEventListener('click', function(e) {
+            e.preventDefault();
+            
+            var productId = this.getAttribute('data-package-product-id');
+            
+            if (!productId) {
+                console.error('Product ID not found');
+                return;
+            }
+            
+            Cart.add({
+                items: {
+                    [productId]: 1
+                }
+            });
+        });
+    }
+});
